8b07d7bd3ddf8924e1da53b5bb98079269c9e5ae
Ceshine Lee <shuanck@gmail.com>
2019-12-12 17:36:19 +0800

    Compatibility with half precision

--- fragile/bot.py
+++ fragile/bot.py
@@ -127,8 +127,8 @@ class BasicQABot(BaseBot):
                 weights.append(y_local.size(self.batch_dim))
                 # print((output["logit_sa"][0, y_local[0, 1], 0]))
                 # print((output["logit_sa"][0, y_local[0, 2], 1]))
-                assert(output["logit_sa"][0, y_local[0, 1], 0] > -1e19)
-                assert(output["logit_sa"][0, y_local[0, 2], 1] > -1e19)
+                assert(output["logit_sa"][0, y_local[0, 1], 0] > -1e9)
+                assert(output["logit_sa"][0, y_local[0, 2], 1] > -1e9)
                 # Save batch labels and predictions
                 # shape (batch, 2)
                 preds_sa.append(
